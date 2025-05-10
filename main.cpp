6091e23c9c089f3468f2a4124d5e1ca5c465c1c8
Roma_home <26rus_mri@mail.ru>
2025-05-10 20:36:11 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -8,10 +8,17 @@
 
 int main(int argc, char *argv[])
 {
+
+	qdbg << "сохранять ширину колонок";
+	qdbg << "дома из-за увеличенных шрифтов есть некоторые уродства";
+	qdbg << "экзешнику картинку";
+
 	QApplication a(argc, argv);
 	a.setQuitOnLastWindowClosed(false);
 
-	Resources::Init("C:\\Work\\C++\\Notes\\resources", MyQDifferent::PathToExe()+"\\files\\resources", true);
+	//Resources::Init("C:\\Work\\C++\\Notes\\resources", MyQDifferent::PathToExe()+"\\files\\resources", true);
+	Resources::Init("D:\\Documents\\C++ QT\\Notes\\resources", MyQDifferent::PathToExe()+"\\files\\resources", true);
+	#error
 
 	WidgetMain w;
 	w.show();
